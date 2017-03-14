95f1e649d00a99e8ce632cbafdc6de3ca408a18a
Vignesh Hari <vichuhari100@gmail.com>
2017-03-14 23:53:40 +0530

    First commit

---
+++ app/src/main/java/net/gotev/project/App.java
@@ -0,0 +1,19 @@
+package net.gotev.project;
+
+import android.app.Application;
+
+import net.gotev.speech.Logger;
+import net.gotev.speech.Speech;
+
+/**
+ * @author Aleksandar Gotev
+ */
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Speech.init(this, getPackageName());
+        Logger.setLogLevel(Logger.LogLevel.DEBUG);
+    }
+}
